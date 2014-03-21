4609cbaab0cdb01ee06368888c917c6026c20477
James Gagan <james@highwaythreesolutions.com>
2014-03-21 20:53:25 +0000

    First commit of edc theme extension for ckan.

---
+++ ckan/ckanext/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(
+    name='ckanext-edc_theme',
+    version=version,
+    description="",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.edc_theme'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        # Add plugins here, e.g.
+        # myplugin=ckanext.edc_theme.plugin:PluginClass
+        edc_theme=ckanext.edc_theme.plugin:ExampleThemePlugin
+    ''',
+)
