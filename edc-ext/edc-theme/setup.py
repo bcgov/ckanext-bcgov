5e0020ce4d514a0a97bfd3841a390a359bb7cc4d
Jereld Hannis <jer@highwaythreesolutions.com>
2014-08-26 20:29:19 +0000

    issue 666 renamed extensions and requirements file

---
+++ edc-ext/edc-theme/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.9.0'
+
+setup(
+    name='ckanext-edc-theme',
+    version=version,
+    description="Provides the look and feel of the DataBC site for CKAN.",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Devin Lumley',
+    author_email='devin@highwaythreesolutions.com',
+    url='highwaythreesolutions.com',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.edc_theme'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        edc_theme=ckanext.edc_theme.plugin:EDCThemePlugin
+    ''',
+)
