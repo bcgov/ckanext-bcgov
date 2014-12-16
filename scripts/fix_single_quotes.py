f5c786b061b24f90f4293a8db16a73efd86f8478
Khalegh Mamakani <khalegh@highwaythreesolutions.com>
2014-12-16 23:18:52 +0000

    Added a python script to fix record description

---
+++ scripts/fix_single_quotes.py
@@ -0,0 +1,57 @@
+
+import urllib2
+import urllib
+import json
+import pprint
+import sys
+
+from base import (site_url, api_key)
+
+records = []
+records_filename = './data/records_with_html_errors.txt'
+with open(records_filename) as records_file :
+    records = records_file.read().splitlines()
+
+
+print 'Fixing description for records containing [HTML_REMOVED] ...'
+count = 0  
+
+for record in records :
+    pkg_id = record.strip()
+    data_string = urllib.quote(json.dumps({'id': pkg_id}))
+    try:
+        request = urllib2.Request(site_url + '/api/3/action/package_show')
+        request.add_header('Authorization', api_key)
+        response = urllib2.urlopen(request, data_string)
+        assert response.code == 200
+         
+        response_dict = json.loads(response.read())
+        assert response_dict['success'] is True
+        pkg_dict = response_dict['result']
+    except Exception:
+        print 'Record with name %s not found.' %(pkg_id)
+        continue
+    if pkg_dict :
+        description = pkg_dict.get('notes')
+        
+        if not '&apos;' in description :
+            continue
+        
+        pkg_dict['notes'] = description.replace('&apos;', "'")
+        
+        data_string = urllib.quote(json.dumps(pkg_dict))
+        try:
+            request = urllib2.Request(site_url + '/api/3/action/package_update')
+            request.add_header('Authorization', api_key)
+            response = urllib2.urlopen(request, data_string)
+            assert response.code == 200
+         
+            response_dict = json.loads(response.read())
+            assert response_dict['success'] is True
+            pkg_dict = response_dict['result']
+            print '.'
+            count += 1
+        except Exception:
+            pass
+
+print count, ' records were updated.'        
