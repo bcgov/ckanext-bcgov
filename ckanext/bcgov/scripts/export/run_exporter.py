91adec18adf54f4d6de5fe16c7ae5df5c126837b
Mathew Brown <mathew.brown@gov.bc.ca>
2015-12-08 14:13:55 -0800

    fixed exporter script. Key needs to be entered as argument to run update_datastore_content

--- ckanext/bcgov/scripts/export/run_exporter.py
+++ ckanext/bcgov/scripts/export/run_exporter.py
@@ -1,8 +1,8 @@
 import exporter
 with open('catalogue.csv', 'w') as f:
-	csv_string = exporter.export('http://catalogue.data.gov.bc.ca', 'columns.json')
+	csv_string = exporter.export('https://catalogue.data.gov.bc.ca', 'columns.json')
 	f.write(csv_string)
 
 #parse into data type files
 import dataset_export
-import update_datastore_content
+import update_datastore_content #enter key as an argument
