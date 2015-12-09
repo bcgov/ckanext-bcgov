f9cc832d0833ebd7dd43189ff10a9e745b645af2
Mathew Brown <mathew.brown@gov.bc.ca>
2015-12-09 08:52:44 -0800

    removed export/ so this branch only includes dmf fix

--- ckanext/bcgov/scripts/export/run_exporter.py
+++
@@ -1,8 +0,0 @@
-import exporter
-with open('catalogue.csv', 'w') as f:
-	csv_string = exporter.export('https://catalogue.data.gov.bc.ca', 'columns.json')
-	f.write(csv_string)
-
-#parse into data type files
-import dataset_export
-import update_datastore_content #enter key as an argument
