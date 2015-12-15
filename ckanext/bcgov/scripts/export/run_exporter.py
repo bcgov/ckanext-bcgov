8a07790d1db7041ba66d17c008e0be0ec938e59e
Mathew Brown <mathew.brown@gov.bc.ca>
2015-12-15 13:38:34 -0800

    updated exporter to run from any env

--- ckanext/bcgov/scripts/export/run_exporter.py
+++ ckanext/bcgov/scripts/export/run_exporter.py
@@ -1,8 +1,19 @@
-import exporter
-with open('catalogue.csv', 'w') as f:
-	csv_string = exporter.export('https://catalogue.data.gov.bc.ca', 'columns.json')
-	f.write(csv_string)
+# run using env as parameter: python run_exporter.py cad id api_key
 
+import exporter
 #parse into data type files
 import dataset_export
 import update_datastore_content #enter key as an argument
+
+
+from sys import argv
+
+script, env, res_id, api_key = argv
+
+with open(env + '.csv', 'w') as f:
+	csv_string = exporter.export('https://' + env + '.data.gov.bc.ca', 'columns.json')
+	f.write(csv_string)
+
+if __name__ == '__main__':
+	dataset_export.export_type(env)
+	update_datastore_content.update_resource(env, res_id, api_key)
