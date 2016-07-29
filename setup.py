2537f593f03b5ea45e6b5fd641b7f8403e1ff582
Leo <leo.lou@gov.bc.ca>
2016-07-28 17:24:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'ckan==2.5.3b',
+        'ckan==2.5.3-bcgov',
         'pycurl==7.19.5',
         'validate_email==1.2',
     ],
