989d5dbb4aa14ee680e4c39bd1a3f987d24c7882
Jared Smith <jared.smith.jrod@gmail.com>
2016-10-28 16:16:13 -0700

    Added building url for ofi api calls

--- ckanext/bcgov/controllers/ofi.py
+++ ckanext/bcgov/controllers/ofi.py
@@ -10,6 +10,7 @@ import cgi
 import logging
 import requests as reqs
 from pprint import pformat, pprint
+import urlparse
 
 # import ckan
 import ckan.lib.base as base
@@ -20,6 +21,7 @@ import ckan.plugins.toolkit as toolkit
 import ckanext.bcgov.util.helpers as edc_h
 
 from ckan.common import _, c, request, response
+from paste.deploy.converters import asbool
 try:
     # CKAN 2.7 and later
     from ckan.common import config
@@ -51,13 +53,19 @@ def log_response(resp, call_type):
 
 class EDCOfiController(ApiController):
     def __init__(self):
-        self._config = edc_h.get_ofi_config()
-        self.ofi_url = self._build_url()
+        self.config = edc_h.get_ofi_config()
         self.cookies = {}
         self.cookies[u'SMSESSION'] = request.cookies.get(u'SMSESSION', '')
-        self.query_params = {}
 #       for key, value in request.cookies.iteritems():
 #            self.cookies[key] = value
+        self.query_params = {}
+        try:
+            self.query_params = self._get_request_data(try_url_params=True)
+        except ValueError, inst:
+            log.info('Bad Action API request data: %s', inst)
+            return self._finish_bad_request(_('JSON Error: %s') % inst)
+
+        self.ofi_url = self._build_url()
 
     def ofi(self, call_action, object_name=None, ver=None):
         '''
@@ -75,15 +83,9 @@ class EDCOfiController(ApiController):
 
         log.debug(u'OFI user cookie: %s', pformat(self.cookies))
 
-        log.debug(u'OFI config:\n %s \n', pformat(self._config))
+        log.debug(u'OFI config:\n %s \n', pformat(self.config))
 
         log.debug(u'OFI api context:\n %s\n', pformat(context))
-
-        try:
-            self.query_params = self._get_request_data(try_url_params=True)
-        except ValueError, inst:
-            log.info('Bad Action API request data: %s', inst)
-            return self._finish_bad_request(_('JSON Error: %s') % inst)
 
         if call_action == 'check_object_name':
             return self._check_object_name()
@@ -95,7 +97,7 @@ class EDCOfiController(ApiController):
         OFI API Call:
         TODO
         '''
-        resp = reqs.get(self.ofi_url, cookies=self.cookies)
+        resp = reqs.get(self.ofi_url + u'/security/productAllowedByFeatureType/WHSE_MINERAL_TENURE.MINPOT_MINERAL_POTENTIAL', cookies=self.cookies)
         content_type = resp.headers.get(u'content-type').split(';')[0]
 
         log_response(resp, u'secure')
@@ -104,7 +106,7 @@ class EDCOfiController(ApiController):
         log.debug(u'OFI api cookie SMSESSION: %s', resp.cookies.get(u'SMSESSION', u''))
 
         if self.ofi_url != resp.url or content_type == 'text/html':
-            return self._finish_not_authz(_('Need valid IDIR Login session'))
+            return self._finish_not_authz(_(u'Need valid IDIR Login session'))
         else:
             return self._finish(resp.status_code, resp.json(), content_type='json')
 
@@ -112,6 +114,17 @@ class EDCOfiController(ApiController):
         '''
         TODO
         '''
-        url = u'https://delivery.apps.gov.bc.ca/pub/dwds-ofi/secure/security/productAllowedByFeatureType/WHSE_MINERAL_TENURE.MINPOT_MINERAL_POTENTIAL'
+        secure = 'secure' in self.query_params
+
+        protocol = self.config.get(u'api.protocol')
+        domain = self.config.get(u'api.hostname')
+        port = self.config.get(u'api.port', '')
+
+        if port != '':
+            domain = u'{}:{}'.format(domain, port)
+
+        order_path = self.config.get(u'api.order_secure_path') if secure else self.config.get(u'api.order_path')
+
+        url = urlparse.urlunparse((protocol, domain, order_path, '', '', ''))
         log.debug(u'OFI API URL: %s', url)
         return url
