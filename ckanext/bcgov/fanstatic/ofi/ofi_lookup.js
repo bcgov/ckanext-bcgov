9e275b54c54d41bce794d511a05437458cae1b97
Jared Smith <jared.smith.jrod@gmail.com>
2016-11-03 10:47:29 -0700

    Cutting out js logic

--- ckanext/bcgov/fanstatic/ofi/ofi_lookup.js
+++ ckanext/bcgov/fanstatic/ofi/ofi_lookup.js
@@ -7,12 +7,6 @@
  *
  * OFI Prototype
  *
- * config options:
- *   bcgov.ofi.protocol          - The protocol to use eg. 'https' or 'http'
- *   bcgov.ofi.hostname          - Specifies the host to call for OFI
- *   bcgov.ofi.port              - Specify the port to use
- *   bcgov.ofi.order_path        - Specify the order path for OFI
- *   bcgov.ofi.order_secure_path - Specify the secure order path
 **/
 "use strict";
 
@@ -22,11 +16,7 @@ this.ckan.module('ofi_lookup', function($, _) {
   return {
     options: {
       // defaults
-      protocol: 'https',
-      hostname: 'delivery.apps.gov.bc.ca',
-      port: '',
-      order_path: '/pub/dwds-ofi/',
-      order_secure_path: '/pub/dwds-ofi/secure/',
+
     },
     initialize: function() {
       self = this;
@@ -36,40 +26,22 @@ this.ckan.module('ofi_lookup', function($, _) {
       spinner = this.$('#loading');
       object_name = this.options.object_name;
 
+      console.log(this.options.ofi_api);
+
       // when the page refreshes, object_name go back to what's specified in the package
       this.$('#object-name').val(object_name);
       this.$('#add-resource').on('click', this._copyResourceToForm);
       ofi_form.submit(this.searchForObject);
 
       if (object_name) {
-        this._callOFIService("security/productAllowedByFeatureType/" + object_name, true, true);
         modal.modal('show');
       } else {
         console.log("No object name in package.");
       }
     },
-    _createURL: function(webService, isSecure) {
-      var o = this.options;
-      return o.protocol + "://"
-              + o.hostname
-              + ((o.port === "" || o.port === undefined) ? "" : ":" + o.port)
-              + ((isSecure) ? o.order_secure_path : o.order_path)
-              + webService;
-    },
-    _copyResourceToForm: function() {
-      console.log("TODO add resource info to the form");
-
-      modal.modal('hide');
-    },
     searchForObject: function(event) {
       event.preventDefault();
 
-      console.log('Making secure call:', this.secure.checked);
-      console.log('Including credentials:', this.include_creds.checked);
-
-      self._callOFIService("security/productAllowedByFeatureType/" + this.object_name.value,
-        this.secure.checked,
-        this.include_creds.checked);
       modal.modal('show');
     },
     _toggleSpinner: function(on_off) {
@@ -79,49 +51,9 @@ this.ckan.module('ofi_lookup', function($, _) {
       resources_show.toggleClass('hidden', on_off);
       spinner.toggleClass('enable', on_off);
     },
-    _callOFIService: function(service_name, is_secure, include_creds) {
-      var httpRequest = new XMLHttpRequest();
-      var ofi_url = this._createURL(service_name, is_secure);
-
-      httpRequest.timeout = 30000; // ms
-      httpRequest.withCredentials = include_creds;
-
-      httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState == XMLHttpRequest.OPENED) {
-          console.log('Calling:', ofi_url);
-          self._toggleSpinner(true); // turn on
-        }
-
-        if (httpRequest.readyState == XMLHttpRequest.DONE) {
-          console.log(httpRequest);
-
-          // testing, TODO: use http status codes for error handling
-          if (httpRequest.responseText) {
-            console.log(httpRequest.responseText);
-            var data = JSON.parse(httpRequest.responseText);
-            self._showResults(data);
-          } else {
-            self._showResults(`Please open dev tools, api call did not work.
-              <br /><br />
-              Expecting error:
-              <br />
-              * Cross-Origin Request Blocked -> Reason: CORS header 'Access-Control-Allow-Origin' does not mach '*'
-              `);
-          }
-        }
-      };
-
-      console.log(ofi_url);
-      httpRequest.open('GET', ofi_url, true);
-      httpRequest.setRequestHeader('Accept', '*/*');
-      httpRequest.send();
-    },
     _showResults: function(data) {
-      if (typeof data == 'object') {
-        resources_show.html(object_name + " is " + ( !data['allowed'] ? "not" : "") + " available.");
-      } else {
-        resources_show.html('<div><p>' + data + '</p></div>');
-      }
+      resources_show.html("");
+
       this._toggleSpinner(false); // turn off
     },
     teardown: function() {
