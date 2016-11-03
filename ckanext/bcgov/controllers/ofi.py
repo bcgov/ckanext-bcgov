a622f36167042dc222060d4bb8d5117fcf609474
Jared Smith <jared.smith.jrod@gmail.com>
2016-11-03 11:52:01 -0700

    Removing ofi controller action logic

--- ckanext/bcgov/controllers/ofi.py
+++ ckanext/bcgov/controllers/ofi.py
@@ -21,7 +21,7 @@ import ckan.plugins.toolkit as toolkit
 import ckanext.bcgov.util.helpers as edc_h
 
 from ckan.common import _, c, request, response
-from paste.deploy.converters import asbool
+
 try:
     # CKAN 2.7 and later
     from ckan.common import config
@@ -38,36 +38,8 @@ ValidationError = logic.ValidationError
 log = logging.getLogger(u'ckanext.bcgov.controllers.ofi')
 
 
-def log_response(resp, call_type):
-    log.debug(u'OFI check object name, call type: %s', call_type)
-    log.debug(u'OFI check object name, url: %s', resp.url)
-    log.debug(u'OFI api response:\n %s', pformat({
-        u'status': resp.status_code,
-        u'reason': resp.reason,
-        u'headers': resp.headers,
-        u'cookies': resp.cookies,
-        u'elapsed': str(resp.elapsed.total_seconds()) + u's'
-    }))
-    log.debug(u'OFI api content: %s', pformat(resp.text))
-
-
 class EDCOfiController(ApiController):
-    def __init__(self):
-        self.config = edc_h.get_ofi_config()
-        self.cookies = {}
-        self.cookies[u'SMSESSION'] = request.cookies.get(u'SMSESSION', '')
-#       for key, value in request.cookies.iteritems():
-#            self.cookies[key] = value
-        self.query_params = {}
-        try:
-            self.query_params = self._get_request_data(try_url_params=True)
-        except ValueError, inst:
-            log.info('Bad Action API request data: %s', inst)
-            return self._finish_bad_request(_('JSON Error: %s') % inst)
-
-        self.ofi_url = self._build_url()
-
-    def ofi(self, call_action, object_name=None, ver=None):
+    def action(self, call_action, object_name=None, ver=None):
         '''
         API Entry
         TODO
@@ -77,9 +49,17 @@ class EDCOfiController(ApiController):
             u'session': model.Session,
             u'user': c.user,
             u'api_version': ver,
-            u'id': id,
             u'auth_user_obj': c.userobj
         }
+
+        try:
+            function = get_action(call_action)
+        except KeyError:
+            return self._finish_bad_request(_('Action name not known: %s') % call_action)
+
+        side_effect_free = getattr(function, 'side_effect_free', False)
+
+        print(side_effect_free)
 
         log.debug(u'OFI user cookie: %s', pformat(self.cookies))
 
@@ -88,46 +68,37 @@ class EDCOfiController(ApiController):
         log.debug(u'OFI api context:\n %s\n', pformat(context))
 
         if call_action == 'check_object_name':
-            return self._check_object_name()
+            pass
+        elif call_action == 'file_formats':
+            pass
+        elif call_action == 'show':
+            pass
         else:
-            return base.abort(400, _('Bad call action: %s') % call_action)
+            return base.abort(501, _('TODO in OFI Controller: %s') % call_action)
 
-    def _check_object_name(self):
-        '''
-        OFI API Call:
-        TODO
-        '''
-        resp = reqs.get(self.ofi_url + u'/security/productAllowedByFeatureType/WHSE_MINERAL_TENURE.MINPOT_MINERAL_POTENTIAL', cookies=self.cookies)
-        content_type = resp.headers.get(u'content-type').split(';')[0]
-
-        log_response(resp, u'secure')
-
-        log.debug(u'Inital SMSESSION: %s', request.cookies.get(u'SMSESSION', u''))
-        log.debug(u'OFI api cookie SMSESSION: %s', resp.cookies.get(u'SMSESSION', u''))
-
-        if self.ofi_url != resp.url or content_type == 'text/html':
-            return self._finish_not_authz(_(u'Need valid IDIR Login session'))
-        else:
-            return self._finish(resp.status_code, resp.json(), content_type='json')
-
-    def _build_url(self):
-        '''
-        TODO
-        '''
-        secure = 'secure' in self.query_params
-
-        protocol = self.config.get(u'api.protocol', 'https')
-        domain = self.config.get(u'api.hostname', 'delivery.apps.gov.bc.ca')
-        port = self.config.get(u'api.port', '')
-
-        if port != '':
-            domain = u'{}:{}'.format(domain, port)
-
-        if secure:
-            order_path = self.config.get(u'api.order_secure_path', '/pub/dwds-ofi/secure')
-        else:
-            order_path = self.config.get(u'api.order_path', '/pub/dwds-ofi')
-
-        url = urlparse.urlunparse((protocol, domain, order_path, '', '', ''))
-        log.debug(u'OFI API URL: %s', url)
-        return url
+    def _call_action_show(self):
+        action_map = {
+            u'info': 'OFI ckan api call_actions',
+            u'call_action': {
+                u'check_object_name': {
+                    u'about': u'Api call to OFI to check Object availability.',
+                    u'url': h.url_for('ofi api', call_action='check_object_name'),
+                    u'params': {
+                        u'object_name': u'',
+                        u'secure': u''
+                    }
+                },
+                u'file_formats': {
+                    u'about': u'List of available file formats.',
+                    u'url': h.url_for('ofi api', call_action='file_formats')
+                },
+                u'create_order': {
+                    u'about': u'TODO: Create an order to OFI',
+                    u'url': h.url_for('ofi api', call_action='create_order'),
+                    u'params': {
+                        u'test': u''
+                    }
+                }
+            }
+        }
+        return self._finish_ok(action_map)
