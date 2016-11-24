86c8c879d8ac52e571faf911e5aab1eba84d5d85
Brock Anderson <brock@bandersgeo.ca>
2016-11-23 16:02:29 -0800

    removed static resource for leaflet.  instead this dependency is loaded from the geoview extension.  moved the leaflet-draw dependency into the fanstatic folder.  all js and css resources are now loaded with the resoruce tag.  removed the example data passing to the edc_mow.js.

--- ckanext/bcgov/fanstatic/edc_mow.js
+++ ckanext/bcgov/fanstatic/edc_mow.js
@@ -14,17 +14,24 @@ this.ckan.module('edc_mow', function($, _) {
 
     var _map = null;
     var _maxAreaHectares = null;
+    var _initialCenterLatLon = [53.5, -128];
+    var _initialZoom = 4;
 
-    var _getMaxDownloadArea = function(callbackSuccess, callbackErr) {
+    var _fetchMaxDownloadableArea = function(callbackSuccess, callbackErr) {
+
+      //max downloadable area resource:
+      //  https://cad.data.gov.bc.ca/api/3/action/resource_show?id=6f370640-fe58-4aa9-832a-f6f615d5a335
+
+      if (_maxAreaHectares){
+        callbackSuccess();
+        return;
+      }
+      
       _toggleSpinner(true);
-
-      //https://cad.data.gov.bc.ca/api/3/action/resource_show?id=6f370640-fe58-4aa9-832a-f6f615d5a335
 
       //simulate async ajax call to fetch the dataset with the maximum 
       //downloadable area for this dataset
-      setTimeout(function() {
-      
-
+      setTimeout(function() {      
         console.log("todo: fetch the resource that gives the max downloadable area for this dataset.  for now just assume a max area of 5000000 hectares");
         _maxAreaHectares = 5000000;
         $('.max-area-hectares').html(_formatNum(_maxAreaHectares));
@@ -59,29 +66,38 @@ this.ckan.module('edc_mow', function($, _) {
 
     var _showMap = function() {
 
-      var bcgovRoadsLayer =  
-        L.tileLayer(
-            'http://maps.gov.bc.ca/arcserver/rest/services/province/roads_wm/MapServer/tile/{z}/{y}/{x}', {
-            attribution: '&copy; Government of British Columbia',
-            minZoom: 4,
-            maxZoom: 17,
-            });
+      if (!_map) {
+        var bcgovRoadsLayer =  
+          L.tileLayer(
+              'http://maps.gov.bc.ca/arcserver/rest/services/province/roads_wm/MapServer/tile/{z}/{y}/{x}', {
+              attribution: '&copy; Government of British Columbia',
+              minZoom: 4,
+              maxZoom: 17,
+              });
 
-      var initialCenterLatLon = [53.5, -128];
-      var initialZoom = 4;
-      _map = L.map('map', {layers: [bcgovRoadsLayer]}) 
-      
-      _map.on("zoomend", _mapViewChanged)
-      _map.on("moveend", _mapViewChanged)
-      
-      _map.setView(initialCenterLatLon, initialZoom);
+        _map = L.map('map', {layers: [bcgovRoadsLayer]}) 
+        
+        _map.on("zoomend", _mapViewChanged)
+        _map.on("moveend", _mapViewChanged)
+      }
+      _map.setView(_initialCenterLatLon, _initialZoom);
+
+    }
+
+    var _initStart = function() {
+      $("#mow-ready").hide();
+      $("#mow-err").hide();
+      _fetchMaxDownloadableArea(_initSuccess, _initFailed)
     }
 
     var _initSuccess = function() {
       $("#mow-ready").show();
       $("#mow-err").hide();
 
-      modal_controls.find('#order-btn').on('click', _placeOrder);
+      //listen to order button clicks
+      var orderBtn = modal_controls.find('#order-btn')
+      orderBtn.off('click');
+      orderBtn.on('click', _placeOrder);
 
       _showMap();
     }
@@ -117,10 +133,10 @@ this.ckan.module('edc_mow', function($, _) {
 
             //capture the bootstrap event fired when the modal window is actually
             //shown.  
-            //at this time invalidate the map to ensure it fills the expected 
-            //screen real estate
+            //perform any initialization that can't be done until the modal window is actually
+            //visible
             $("#edc-mow").on("shown.bs.tab", function() {
-                _getMaxDownloadArea(_initSuccess, _initFailed)
+              _initStart();
             });
 
         },
