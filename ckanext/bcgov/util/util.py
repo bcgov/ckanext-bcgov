47602d4afc4c897eb7f7da36b151a304c76f07e4
garrett <garrett@highwaythreesolutions.com>
2018-01-25 10:17:02 -0800

    Init datastore downloads integration

--- ckanext/bcgov/util/util.py
+++ ckanext/bcgov/util/util.py
@@ -477,7 +477,27 @@ def can_view_resource(resource):
     return False
 
 def get_package_tracking(package_id):
-    return (model.TrackingSummary.get_for_package(package_id))
+    # dl_resource_id = 'e0d0e432-0f86-4db9-8ea7-5a4e002c5c0f'
+    dl_resource_id = '1a7df2c1-f7df-46f1-a920-da85c04eeab6'
+    
+    try:
+        request = urllib2.Request(site_url + '/api/3/action/datastore_search?resource_id'+dl_resource_id+'&q='+package_id)
+        request.add_header('Authorization', api_key)
+        response = urllib2.urlopen(request, '')
+        assert response.code == 200
 
+        # Use the json module to load CKAN's response into a dictionary.
+        response_dict = response.read()
+        log.info(response_dict)
+        assert response_dict['success'] is True
+
+
+    except Exception, e:
+        log.info(e)
+        print str(e)
+        pass
+
+    return (model.TrackingSummary.get_for_package(package_id))
+    
 def get_resource_tracking(resource_url):
     return (model.TrackingSummary.get_for_resource(resource_url))
