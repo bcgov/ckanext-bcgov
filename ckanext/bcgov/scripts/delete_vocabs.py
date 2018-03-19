d0633235deaec62e596de6cc6b2a8f37d125c08d
Jared Smith <jared.smith.jrod@gmail.com>
2018-03-19 16:22:26 -0700

    Updated edc_tags.py script tool for updating, creating and deleting

--- ckanext/bcgov/scripts/delete_vocabs.py
+++ ckanext/bcgov/scripts/delete_vocabs.py
@@ -11,45 +11,45 @@ from base import (site_url, api_key)
 def delete_vocabs():
 
     print 'Deleting all vocabularies ....'
-    #Get the list of all vocabs
-    
+    # Get the list of all vocabs
+
     vocabs_list = []
     try:
         request = urllib2.Request(site_url + '/api/3/action/vocabulary_list')
         response = urllib2.urlopen(request)
         assert response.code == 200
-            
+
         response_dict = json.loads(response.read())
         assert response_dict['success'] is True
 
         vocabs_list = response_dict['result']
     except Exception:
-        pass 
-    
+        pass
+
     for vocab in vocabs_list:
-        try :
-            #Deleting all tags of the current vocab
-            for tag in vocab.get('tags') :
-                data_string =  urllib.quote(json.dumps({'id' : tag['id'], 'vocabulary_id' : vocab['id']}))
+        try:
+            # Deleting all tags of the current vocab
+            for tag in vocab.get('tags'):
+                data_string = urllib.quote(json.dumps({'id' : tag['id'], 'vocabulary_id': vocab['id']}))
                 request = urllib2.Request(site_url + '/api/3/action/tag_delete')
                 request.add_header('Authorization', api_key)
                 response = urllib2.urlopen(request, data_string)
                 assert response.code == 200
                 response_dict = json.loads(response.read())
                 assert response_dict['success'] is True
-                
-            #Deleting the vocabulary
-            data_string =  urllib.quote(json.dumps({'id' : vocab['id']}))                        
+
+            # Deleting the vocabulary
+            data_string = urllib.quote(json.dumps({'id': vocab['id']}))
             request = urllib2.Request(site_url + '/api/3/action/vocabulary_delete')
             request.add_header('Authorization', api_key)
             response = urllib2.urlopen(request, data_string)
             assert response.code == 200
             response_dict = json.loads(response.read())
             assert response_dict['success'] is True
-            
+
         except Exception:
             pass
-        
+
     print 'Done.'
 
 delete_vocabs()
--- ckanext/bcgov/scripts/edc_tags.py
+++ ckanext/bcgov/scripts/edc_tags.py
@@ -1,26 +1,23 @@
 """edc_tags command line interface
 
 Usage:
-    edc_tags update <vocab_list> (-r SITE_URL) [-a APIKEY] [ -f ]
+    edc_tags <action> [VOCAB ...] (-r SITE_URL) [options]
 
 Arguments:
-    -r --remote=URL           URL of CKAN server for remote actions
+    <action>                Update, Create or Delete
+    [VOCAB ...]             List of vocabularys by name or id, if no VOCAB is
+                             is specified, all vocab items in the data-file
+                             will be used.
+    -r --remote=URL         URL of CKAN server for remote actions
 
 Options:
-    -h --help                 Show Usage
-    --version                 Show version
-    -a --apikey=APIKEY        API key to use for remote actions
-    -f --all-fields           update: option for getting all info from fields
-                               in the vocabulary list, default is True
-
-Todo:
-    Add other commands like create and delete
-    If no <vocab_list> is provided, should update all lists
-
-    Add this option:
-    -d --data-file            File location for the Vocabulary list,
-                               default is ./data/edc-vocabs.json in
-                               ckanext-bcgov
+    -h --help               Show Usage
+    --version               Show version
+    -a, --apikey=APIKEY     API key to use for remote actions
+    -d, --data-file=FILE    File location for the Vocabulary list
+                             [default: ./data/edc-vocabs.json]
+    -f, --all-fields        For getting all info from fields in the
+                             vocabulary list [default: True]
 """
 import json
 
@@ -30,37 +27,90 @@ from ckanapi import RemoteCKAN
 
 args = docopt(__doc__, version='1')
 
+action = args.get('<action>')
+vocab_names = args.get('VOCAB')
+
 url = args.get('--remote')
 api_key = args.get('--apikey')
 all_fields = args.get('--all-fields', True)
-vocab_name = args.get('<vocab_list>')
-vocab_id = ''
-edc_vocab_location = ''
+
+edc_vocab_location = args.get('--data-file')
 edc_vocabs = None
 
 
-with open('./data/edc-vocabs.json') as json_file:
-    edc_vocabs = json.loads(json_file.read())
+def _get_vocab_list(vocab_name):
+    for i in edc_vocabs:
+        if i['name'] == vocab_name:
+            return i['tags']
 
 
-with RemoteCKAN(url, apikey=api_key) as api:
+def _new_tags(api, vocab_id, vocab_name):
+    new_vocab_list = _get_vocab_list(vocab_name)
+
+    for new_item in new_vocab_list:
+        tag = api.action.tag_create(name=new_item, vocabulary_id=vocab_id)
+        yield tag
+        print(tag)
+
+
+def create_tags(api, vocab_name):
+    print("Creating vocabulary %s @ %s ..." % (vocab_name, url))
+
+    created = api.action.vocabulary_create(name=vocab_name)
+    print(created)
+    vocab_id = created['id']
+
+    print('New vocabulary list %s created.' % created['name'])
+
+    new_tags = [tag for tag in _new_tags(api, vocab_id, vocab_name)]
+
+    print("Finished creating %s.\n" % vocab_name)
+
+
+def update_tags(api, vocab_name):
     print("Updating vocabulary %s @ %s ..." % (vocab_name, url))
+
+    # Need to delete the tag item individually first because
+    # deleting the vocabulary itself by id throws integraty error
+    vocab_id = delete_tags(api, vocab_name, display_msg=False)
+
+    new_tags = [tag for tag in _new_tags(api, vocab_id, vocab_name)]
+
+    print("Finished updating %s.\n" % vocab_name)
+
+
+def delete_tags(api, vocab_name, tag_list=[], display_msg=True):
+    if display_msg:
+        print("Deleting vocabulary %s @ %s ..." % (vocab_name, url))
 
     vocab_id = api.action.vocabulary_show(id=vocab_name).get('id')
 
     tag_list = api.action.tag_list(vocabulary_id=vocab_id,
                                    all_fields=all_fields)
 
-    # Need to delete the tag item individually first because
-    # deleting the vocabulary by id throws integraty error
     for tag in tag_list:
         api.action.tag_delete(id=tag['id'],
                               vocabulary_id=tag['vocabulary_id'])
 
-    new_vocab_list = [j for i in edc_vocabs for j in i['tags'] if i['name'] == vocab_name]
+    if display_msg:
+        print("Finished deleting %s." % vocab_name)
 
-    for new_item in new_vocab_list:
-        created = api.action.tag_create(name=new_item, vocabulary_id=vocab_id)
-        print(created)
+    return vocab_id  # for usage in update_tags
 
-    print("Finished updating %s." % vocab_name)
+
+if __name__ == '__main__':
+    with open(edc_vocab_location) as json_file:
+        edc_vocabs = json.loads(json_file.read())
+
+    with RemoteCKAN(url, apikey=api_key) as api:
+        for vocab_name in vocab_names:
+            if action in ['update', 'u']:
+                update_tags(api, vocab_name)
+            elif action in ['create', 'c']:
+                create_tags(api, vocab_name)
+            elif action in ['delete', 'd']:
+                delete_tags(api, vocab_name)
+            else:
+                print("%s is not an action. "
+                      "Avaiable actions: update | create | delete" % action)
+                exit(1)
