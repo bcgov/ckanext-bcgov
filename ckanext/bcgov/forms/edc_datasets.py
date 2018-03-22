1e23fae27cf52fa4372e9d3300c565f8cb3b6057
Jared Smith <jared.smith.jrod@gmail.com>
2018-03-22 09:59:21 -0700

    Fixed validator for resource_storage_format on other schemas

--- ckanext/bcgov/forms/edc_datasets.py
+++ ckanext/bcgov/forms/edc_datasets.py
@@ -190,7 +190,7 @@ class EDC_GeoSpatialForm(edc_form.EDC_DatasetForm):
             'format': [not_empty, unicode],
             'edc_resource_type': [convert_from_extras, not_empty],
             'resource_storage_access_method': [convert_from_extras],
-            'resource_storage_location': [convert_from_extras, unicode],
+            'resource_storage_location': [convert_from_extras, ignore_missing, unicode],
             'data_collection_start_date': [convert_from_extras, ignore_missing],
             'data_collection_end_date': [convert_from_extras, ignore_missing],
             'projection_name': [convert_from_extras, not_empty],
@@ -232,7 +232,7 @@ class EDC_NonGeoSpatialForm(edc_form.EDC_DatasetForm):
             'format': [not_empty, unicode],
             'edc_resource_type': [not_empty, convert_to_extras],
             'resource_storage_access_method': [not_empty, convert_to_extras],
-            'resource_storage_location': [ignore_missing, unicode, convert_to_extras],
+            'resource_storage_location': [not_empty, unicode, convert_to_extras],
             'data_collection_start_date': [ignore_missing, valid_date, convert_to_extras],
             'data_collection_end_date': [ignore_missing, valid_date, convert_to_extras],
         })
