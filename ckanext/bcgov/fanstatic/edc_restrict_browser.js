812706c8ff1e1e4c6ffba54d2695eecfd431f9b4
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-12-06 14:25:53 -0800

    better checking for chrome

--- ckanext/bcgov/fanstatic/edc_restrict_browser.js
+++ ckanext/bcgov/fanstatic/edc_restrict_browser.js
@@ -25,7 +25,40 @@ function isUsingSupportedBrowser() {
 	var isFirefox = typeof InstallTrigger !== 'undefined';
 
 	// A global object inserted by Chrome
-	var isChrome = !!window.chrome && !!window.chrome.webstore;
+	var isChrome = false;
+
+
+	// please note,
+	// that IE11 now returns undefined again for window.chrome
+	// and new Opera 30 outputs true for window.chrome
+	// but needs to check if window.opr is not undefined
+	// and new IE Edge outputs to true now for window.chrome
+	// and if not iOS Chrome check
+	// so use the below updated condition
+	//as of Chrome 71 chrome no longer has the window.chrome.webstore that used to be the way this checked
+	//https://stackoverflow.com/a/13348618/9627526 has a continuously updated check
+	
+	var isChromium = window.chrome;
+	var winNav = window.navigator;
+	var vendorName = winNav.vendor;
+	var isOpera = typeof window.opr !== "undefined";
+	var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+	var isIOSChrome = winNav.userAgent.match("CriOS");
+
+	if (isIOSChrome) {
+	   // is Google Chrome on IOS
+		isChrome = false;
+	} else if(
+	  isChromium !== null &&
+	  typeof isChromium !== "undefined" &&
+	  vendorName === "Google Inc." &&
+	  isOpera === false &&
+	  isIEedge === false
+	) {
+	   isChrome = true;
+	} else {
+	   isChrome = false;
+	}
 
 	if(isFirefox || isChrome) {
 		return true;
