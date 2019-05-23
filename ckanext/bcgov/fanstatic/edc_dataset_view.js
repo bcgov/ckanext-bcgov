26892672840b30d9074077d95bd0a20d73df09a8
John Churchill <john@highwaythreesolutions.com>
2019-05-23 10:21:46 -0700

    Reversed all changes for permalink modal

--- ckanext/bcgov/fanstatic/edc_dataset_view.js
+++ ckanext/bcgov/fanstatic/edc_dataset_view.js
@@ -1,21 +1,5 @@
 
 (function($) {
     $(document).ready(function() {
-        $('#permalinkLink').click(function(e){
-            $('#permalinkModal').modal();
-            $('#permalinkModal').show();
-            e.preventDefault();
-            e.stopPropagation();
-        });
-
-        $('#permalinkModal span.close').click(function(e){
-            $('#permalinkModal').hide();
-        });
-        
-        $('#copyPermalink').click(function(e) {
-            $('#permalinkLinkInput').select();
-            document.execCommand("copy");
-            $('#permalinkCopiedStatus').innerHTML = "Copied";
-        });
     });
 })(jQuery);
