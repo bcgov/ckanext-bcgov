e53781db3214a468d17b26782691b11899842e98
Brandon Sharratt <brandon@highwaythreesolutions.com>
2019-05-29 15:58:03 -0700

    new browser checking, old banner

--- ckanext/bcgov/fanstatic/edc_restrict_browser.js
+++ ckanext/bcgov/fanstatic/edc_restrict_browser.js
@@ -11,76 +11,212 @@
  * Returns true if current browser is supported,
  * false otherwise.
  *
- * Supported browsers:
- * - Google Chrome
- * - Mozilla Firefox
  *
  * @return {Boolean}
  */
 function isUsingSupportedBrowser() {
 
-	// Detect browser via feature detection rather than userAgent
+	var Browsers, OS, Platform, Versions, browser_name, browser_version, os, platform;
+  Versions = {
+    Firefox: /firefox\/([\d\w\.\-]+)/i,
+    IE: /msie\s([\d\.]+[\d])/i,
+    Chrome: /chrome\/([\d\w\.\-]+)/i,
+    Safari: /version\/([\d\w\.\-]+)/i,
+    Ps3: /([\d\w\.\-]+)\)\s*$/i,
+    Psp: /([\d\w\.\-]+)\)?\s*$/i
+  };
+  Browsers = {
+    Konqueror: /konqueror/i,
+    Chrome: /chrome/i,
+    Safari: /safari/i,
+    IE: /msie/i,
+    Opera: /opera/i,
+    PS3: /playstation 3/i,
+    PSP: /playstation portable/i,
+    Firefox: /firefox/i
+  };
+  OS = {
+    WindowsVista: /windows nt 6\.0/i,
+    Windows7: /windows nt 6\.\d+/i,
+    Windows2003: /windows nt 5\.2/i,
+    WindowsXP: /windows nt 5\.1/i,
+    Windows2000: /windows nt 5\.0/i,
+    OSX: /os x (\d+)[._](\d+)/i,
+    Linux: /linux/i,
+    Wii: /wii/i,
+    PS3: /playstation 3/i,
+    PSP: /playstation portable/i,
+    Ipad: /\(iPad.*os (\d+)[._](\d+)/i,
+    Iphone: /\(iPhone.*os (\d+)[._](\d+)/i
+  };
+  Platform = {
+    Windows: /windows/i,
+    Mac: /macintosh/i,
+    Linux: /linux/i,
+    Wii: /wii/i,
+    Playstation: /playstation/i,
+    Ipad: /ipad/i,
+    Ipod: /ipod/i,
+    Iphone: /iphone/i,
+    Android: /android/i,
+    Blackberry: /blackberry/i
+  };
+  function UserAgent(source) {
+    if (source == null) {
+      source = navigator.userAgent;
+    }
+    this.source = source.replace(/^\s*/, '').replace(/\s*$/, '');
+    this.browser_name = browser_name(this.source);
+    this.browser_version = browser_version(this.source);
+    this.os = os(this.source);
+    this.platform = platform(this.source);
+  }
+  browser_name = function(string) {
+    if (Browsers.Konqueror.test(string)) {
+        return 'konqueror';
+    } else if (Browsers.Chrome.test(string)) {
+        return 'chrome';
+    } else if (Browsers.Safari.test(string)) {
+        return 'safari';
+    } else if (Browsers.IE.test(string)) {
+        return 'ie';
+    } else if (Browsers.Opera.test(string)) {
+        return 'opera';
+    } else if (Browsers.PS3.test(string)) {
+        return 'ps3';
+    } else if (Browsers.PSP.test(string)) {
+        return 'psp';
+    } else if (Browsers.Firefox.test(string)) {
+        return 'firefox';
+    } else {
+        return 'unknown';
+    }
+  };
+  browser_version = function(string) {
+    var regex;
+    switch (browser_name(string)) {
+      case 'chrome':
+        if (Versions.Chrome.test(string)) {
+          return RegExp.$1;
+        }
+        break;
+      case 'safari':
+        if (Versions.Safari.test(string)) {
+          return RegExp.$1;
+        }
+        break;
+      case 'firefox':
+        if (Versions.Firefox.test(string)) {
+          return RegExp.$1;
+        }
+        break;
+      case 'ie':
+        if (Versions.IE.test(string)) {
+          return RegExp.$1;
+        }
+        break;
+      case 'ps3':
+        if (Versions.Ps3.test(string)) {
+          return RegExp.$1;
+        }
+        break;
+      case 'psp':
+        if (Versions.Psp.test(string)) {
+          return RegExp.$1;
+        }
+        break;
+      default:
+        regex = /#\{name\}[\/ ]([\d\w\.\-]+)/i;
+        if (regex.test(string)) {
+          return RegExp.$1;
+        }
+    }
+  };
+  os = function(string) {
+    if (OS.WindowsVista.test(string)) {
+        return 'Windows Vista';
+    } else if (OS.Windows7.test(string)) {
+        return 'Windows 7';
+    } else if (OS.Windows2003.test(string)) {
+        return 'Windows 2003';
+    } else if (OS.WindowsXP.test(string)) {
+        return 'Windows XP';
+    } else if (OS.Windows2000.test(string)) {
+        return 'Windows 2000';
+    } else if (OS.Linux.test(string)) {
+        return 'Linux';
+    } else if (OS.Wii.test(string)) {
+        return 'Wii';
+    } else if (OS.PS3.test(string)) {
+        return 'Playstation';
+    } else if (OS.PSP.test(string)) {
+        return 'Playstation';
+    } else if (OS.OSX.test(string)) {
+        return string.match(OS.OSX)[0].replace('_', '.');
+    } else if (OS.Ipad.test(string)) {
+        return string.match(OS.Ipad)[0].replace('_', '.');
+    } else if (OS.Iphone.test(string)) {
+        return string.match(OS.Iphone)[0].replace('_', '.');
+    } else {
+        return 'unknown';
+    }
+  };
+  platform = function(string) {
+    if (Platform.Windows.test(string)) {
+      return "Microsoft Windows";
+    } else if (Platform.Mac.test(string)) {
+      return "Apple Mac";
+    } else if (Platform.Android.test(string)) {
+      return "Android";
+    } else if (Platform.Blackberry.test(string)) {
+      return "Blackberry";
+    } else if (Platform.Linux.test(string)) {
+      return "Linux";
+    } else if (Platform.Wii.test(string)) {
+      return "Wii";
+    } else if (Platform.Playstation.test(string)) {
+      return "Playstation";
+    } else if (Platform.Ipad.test(string)) {
+      return "iPad";
+    } else if (Platform.Ipod.test(string)) {
+      return "iPod";
+    } else if (Platform.Iphone.test(string)) {
+      return "iPhone";
+    } else {
+      return 'unknown';
+    }
+  };
 
-	// Firefox API to install add-ons
-	var isFirefox = typeof InstallTrigger !== 'undefined';
-
-	// A global object inserted by Chrome
-	var isChrome = false;
-
-
-	// please note,
-	// that IE11 now returns undefined again for window.chrome
-	// and new Opera 30 outputs true for window.chrome
-	// but needs to check if window.opr is not undefined
-	// and new IE Edge outputs to true now for window.chrome
-	// and if not iOS Chrome check
-	// so use the below updated condition
-	//as of Chrome 71 chrome no longer has the window.chrome.webstore that used to be the way this checked
-	//https://stackoverflow.com/a/13348618/9627526 has a continuously updated check
-
-	var isChromium = window.chrome;
-	var winNav = window.navigator;
-	var vendorName = winNav.vendor;
-	var isOpera = typeof window.opr !== "undefined";
-	var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
-	var isIOSChrome = winNav.userAgent.match("CriOS");
-
-	if (isIOSChrome) {
-	   // is Google Chrome on IOS
-		isChrome = false;
-	} else if(
-	  isChromium !== null &&
-	  typeof isChromium !== "undefined" &&
-	  vendorName === "Google Inc." &&
-	  isOpera === false &&
-	  isIEedge === false
-	) {
-	   isChrome = true;
-	} else {
-	   isChrome = false;
-	}
-
-	if(isFirefox || isChrome) {
-		return true;
-	}
-	else {
+    
+	var userAgent = new UserAgent();
+	if (userAgent.browser_name == 'ie') {
 		return false;
 	}
+	if (userAgent.browser_name == 'unknown') {
+        return false;
+	}
+	if (userAgent.browser_name == 'firefox' && userAgent.browser_version < 12) {
+        return false;
+	}
+	if (userAgent.browser_name == 'safari' && userAgent.browser_version < 5) {
+		return false;
+	}
+      return true;
 
 }
 
 $(function() {
 	//Ckanext-bcgov#685 remove check
-	// if(!isUsingSupportedBrowser()) {
+	if(!isUsingSupportedBrowser()) {
 	//
 	// 	// Remove page content
-	// 	var $primaryDiv = $('.primary div.module-content')
+		var $primaryDiv = $('.primary div.module-content')
 	// 	$primaryDiv.html('');
 	//
 	// 	// Insert a warning
-	// 	var warningText = "You are attempting to update or edit BC Data Catalogue content using a non-supported web browser or version. Supported browsers are Google Chrome (downloadable from <a href=\"https://www.google.com/chrome/\" target=\"_blank\">https://www.google.com/chrome/</a>) and Firefox (downloadable from <a href=\"https://www.mozilla.org/en-US/firefox/new/\" target=\"_blank\">https://www.mozilla.org/en_US/firefox/new/</a>). Not all version of these browsers are supported. If you are seeing this message and you are using Firefox or Chrome, then please report this problem to DataBC Catalogue Services.";
-	// 	var $warningDiv = $('<div>' + warningText + "</div>").addClass('alert alert-danger');
-	// 	$primaryDiv.prepend($warningDiv);
+		var warningText = "You are using an unsupported browser, some functionality may not work as expected if you encounter difficulties please update your browser. Supported browsers are <a href=\"https://www.google.com/chrome/\" target=\"_blank\">Google Chrome</a>, <a href=\"https://www.mozilla.org/en-US/firefox/new/\" target=\"_blank\">Firefox</a>, <a href=\"https://www.microsoft.com/en-ca/windows/microsoft-edge\" target=\"_blank\">Edge</a>, and <a href=\"https://support.apple.com/downloads/safari\" target=\"_blank\">Safari</a>";
+		var $warningDiv = $('<div>' + warningText + "</div>").addClass('alert alert-danger');
+		$primaryDiv.prepend($warningDiv);
 	//
-	// }
+	}
 });
